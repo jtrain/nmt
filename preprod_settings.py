79ecf70d7f2f0ec9f1b389291fc31d00a7e89edd
Daniel Hillier <daniel@whit.com.au>
2012-03-27 23:03:59 +1100

    Change preprod url to be dev.dontshowmyteam.com

--- preprod_settings.py
+++ preprod_settings.py
@@ -1,4 +1,4 @@
-APP_URL = "http://test.dontshowmyteam.com/"
+APP_URL = "http://dev.dontshowmyteam.com/"
 DEBUG = False
 DB_NAME = '/var/www/nmt/nmt-preprod.db'
 LOG_FILE = '/var/www/nmt/preprod_scrape_errors.log'
