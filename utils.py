afe6d118ac47fd4cf049e625c5992e5a1e8752b3
Danifus <daniel.hillier@gmail.com>
2013-03-29 23:50:38 +1100

    Add generic sqlite namedtuple factory to utils

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+from collections import namedtuple, defaultdict
+
 import re
 import sqlite3
 
@@ -13,3 +15,12 @@ def make_namedtuple_factory(NamedTuple):
             return row
 
     return named_tuple_factory
+
+def generic_namedtuple_factory(cursor, row):
+    """
+    Usage:
+    con.row_factory = namedtuple_factory
+    """
+    fields = [col[0] for col in cursor.description]
+    Row = namedtuple("Row", fields)
+    return Row(*row)
