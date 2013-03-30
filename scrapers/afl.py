4f321586c045fcc4fa4283788c28130c1a07f827
Daniel Hillier <daniel@whit.com.au>
2013-03-30 13:01:16 +1100

    Update afl scraper to use new soap service

--- scrapers/afl.py
+++ scrapers/afl.py
@@ -3,6 +3,7 @@ Use the afl SOAP web app (see here for details:
 http://xml.afl.com.au/mobilewebservices.asmx) to get the latest game scores.
 """
 from collections import namedtuple
+import datetime
 import json
 import os
 import requests
@@ -11,6 +12,10 @@ import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__),'..'))
 import settings
+import afl_model
+
+ARBITRARY_UID = 11235813
+STARTDATETIME_FMT = "%Y-%m-%dT%H:%M:%SZ"
 
 Game = namedtuple("Game", "home_name home_score away_name away_score")
 
@@ -18,97 +23,82 @@ url = "http://arf.webservice.sportsflash.com.au/WebService.asmx?WSDL"
 
 client = Client(url)
 
-# ========   App codes   =======
+# Process:
+# Get fixture
+# Compute latest round
+# Compute games that need to be fetched
+# Look up teamid from team name
+#  - Scrape team names if necessary
 #
-# ========   SportCodes
-# (returned from GetSportCodes() only listing important ones.):
-#   (SportCode){
-#          Id = 1
-#          Name = "Australian Rules Football"
-#       },
-#   (SportCode){
-#            Id = 5
-#            Name = "Rugby League"
-#         }
-#   (SportCode){
-#            Id = 7
-#            Name = "Rugby Union"
-#         }
-#
-# ========   Competitioncodes
-# (returned from GetSportCompetitionsByCode() only listing important ones.):
-# ====  AFL
-#
-#   (SportCompetition){
-#            Id = 26
-#            Name = "AFL NAB Cup"
-#         },
-#   (SportCompetition){
-#            Id = 1
-#            Name = "AFL Premiership"
-#         }
-#   (SportCompetition){
-#            Id = 22
-#            Name = "AFL Finals Series"
-#         },
-def get_comp_afl():
-    """Return the in progress competition (NAB cup or Finals or whatever)
-    """
-    # Afl premiership = code 1
-    return 1
+def get_fixture_and_store_in_db(conn, client, uid):
+    fixture = client.service.GetFixture(uid)
+    fixture_records = []
+    for record in fixture.Fixture.Event:
+        # Match text = "Carlton vs. Richmond"
+        teams = record.Match.split(' vs. ')
+        startdatetime = datetime.datetime.strptime(record.StartDateTime,
+                                                    STARTDATETIME_FMT)
+        fixture_records.append(afl_model.FixtureRecord(
+                                            seriesId=record._seriesId,
+                                            roundId=record._roundId,
+                                            matchId=record._matchId,
+                                            venueId=record.Venue._venueId,
+                                            team_one=teams[0],
+                                            team_two=teams[1],
+                                            startdatetime=startdatetime
+                                            )
+                                        )
+    afl_model.update_fixture(conn, fixture_records)
 
+def update_and_get_teamid(conn, match):
+    seriesId = match.seriesId
+    result = client.service.GetSeriesTeam(ARBITRARY_UID, seriesId)
+    teams = result.SeriesTeams.Header.Teams.Team
 
-def get_comp(league):
-    """This will select the function to run to get the current competition for
-    the legue specified"""
-    if league == 'afl':
-        return get_comp_afl()
+    name_records = []
+    for team in teams:
+        name_records.append(afl_model.TeamNameRecord(seriesId=seriesId,
+                                                    teamId=team._teamId,
+                                                    longname=team.LongName,
+                                                    shortname=team.ShortName)
+                                                    )
 
+    afl_model.update_teamnamemap(conn, name_records)
 
-def get_current_season(client, competition_id):
-    """Return the unique id for the current season for the competition
-    requested.
+    return afl_model.get_teamid(conn, match.seriesId, match.home_name)
 
-    The competition id can be obtained from 'GetSportCompetitionsByCode()'.
-    A short list of the more important ones are shown above.
-    """
-    result = client.service.GetCurrentSportSeasonByCompetition(competition_id)
-    return result.Id, result.StartDate.year
+def get_teamid(conn, match):
+    teamid = afl_model.get_teamid(conn, match.seriesId,  match.home_name)
 
-def get_current_round_id(client, season_id):
-    """Return the unique round id (it is unique across all sports and comps).
-    """
-    result = client.service.GetCurrentSportRoundBySeason(season_id)
-    return result.Id
+    if not teamid:
+        teamid = update_and_get_teamid(conn, match)
 
-def get_games_from_fixture(client, round_id):
-    """Return all the games for the unique round id key given.
+    return teamid
 
-    home_team, home_score, away_team, away_score
-    """
-    result = client.service.GetFixturesByRound(round_id)
-
-    matches = result.FixtureList[0]
-
-    games = []
-
+def update_aflgames(conn, matches):
     for match in matches:
-        home = match.HomeTeam.Name
-        away = match.AwayTeam.Name
+        teamid = get_teamid(conn, match)
+        matchinfo = client.service.GetCompetitionFixture(ARBITRARY_UID,
+                                                        match.seriesId,
+                                                        match.roundId,
+                                                        teamid,
+                                                        match.venueId)
+        fixture_match = matchinfo.Fixture.Series.Rounds.Round.Matches.Match
 
-        if match.Status == '[undefined]':
-            # If the game hasn't started, use this so the site will show 'vs'
-            home_score = None
-            away_score = None
-        else:
-            home_score = match.HomeScore
-            away_score = match.AwayScore
-            if home_score == 0 and away_score == 0:
-                home_score = away_score = None
+        aflgame = afl_model.AFLGameRecord(seriesId=match.seriesId,
+                                roundId=match.roundId,
+                                matchId=match.matchId,
+                                venueId=match.venueId,
+                                home_name=fixture_match._homeTeamName,
+                                home_score=fixture_match._homeTeamScore,
+                                away_name=fixture_match._awayTeamName,
+                                away_score=fixture_match._awayTeamScore,
+                                currentstatus=fixture_match._currentStatus
+                                )
 
-        games.append(Game(home, home_score, away, away_score))
+        print aflgame
+        afl_model.update_AFLGame(conn, aflgame)
 
-    return games
 
 def store_games_in_db(league, year, weekno, games):
     """
@@ -134,28 +124,25 @@ def store_games_in_db(league, year, weekno, games):
                 'records': json.dumps(records),
                 'key': settings.POST_KEY})
 
+def update_server_afl(conn, league, round):
+
+    year = round.start.year
+    games = afl_model.get_current_round_scores(conn)
+    store_games_in_db('afl', year, round.roundId, games)
+
 def scrape_league(league):
     """Does the scrape of the league and sends it to the db.
 
     Pass in the unque string for the league you want to scrape.
     """
 
-    competition_id = get_comp(league)
-    season_id, year = get_current_season(client, competition_id)
-    current_round_id = get_current_round_id(client, season_id)
-
-    # get games from the fixture table for this week.
-    games = get_games_from_fixture(client, current_round_id)
-
-    # save in the database.
-    store_games_in_db(league, year, current_round_id, games)
-
+    conn = afl_model.create_db_and_get_connection(afl_model.AFL_DB_FILE)
+    get_fixture_and_store_in_db(conn, client, ARBITRARY_UID)
+    round = afl_model.get_round(conn)
+    afl_model.refresh_AFLGame_table_round(conn, round.seriesId, round.roundId)
+    active_games = afl_model.get_active_games(conn)
+    update_aflgames(conn, active_games)
+    update_server_afl(conn, league, round)
 
 if __name__ == "__main__":
-    print client
-    # print client.service.GetCompetitionFixture(11235813, 138, roundid, teamid, venueid)
-    # print client.service.GetCompetitionFixture(11235813, 138, 1, 16, 42)
-    # print client.service.GetCompetitionFixture(11235813, 138, 1, 16, 42)
-    # print client.service.GetFixture(1,138)
-    print client.service.GetSeriesTeam(1,138)
-    # print client.service.GetCompetitionList(1)
+    scrape_league('afl')
--- scrapers/afl_model.py
+++ scrapers/afl_model.py
@@ -1,26 +1,42 @@
 from collections import namedtuple, defaultdict
+import datetime
 import os
 import sqlite3
 import settings
 import sys
 
-sys.path.append(os.path.join(os.path.dirname(__FILE__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from utils import generic_namedtuple_factory
 
 AFL_DB_FILE = 'afl.db'
+AFL_CODE = 'afl'
+
+ONE_DAY = datetime.timedelta(days=1)
 
 FixtureRecord = namedtuple("FixtureRecord", ["seriesId",
                                                 "roundId",
                                                 "matchId",
+                                                "venueId",
                                                 "team_one",
                                                 "team_two",
-                                                "startdatetimeUTC"])
+                                                "startdatetime"])
 
-TeamNameRecord = namedtuple("TeamNameRecord", ["teamId",
+TeamNameRecord = namedtuple("TeamNameRecord", ["seriesId",
+                                                "teamId",
                                                 "longname",
                                                 "shortname"])
 
+AFLGameRecord = namedtuple("AFLGameRecord", ["seriesId",
+                                                "roundId",
+                                                "matchId",
+                                                "venueId",
+                                                "home_name",
+                                                "home_score",
+                                                "away_name",
+                                                "away_score",
+                                                "currentstatus"
+                                                ])
 #--------------------
 # Fixture
 
@@ -30,10 +46,11 @@ create table if not exists Fixture (
  seriesId integer,
  roundId integer,
  matchId integer,
+ venueId integer,
 
  team_one text,
  team_two text,
- startdatetimeUTC datetime,
+ startdatetime timestamp,
 
  primary key (seriesId, roundId, matchId)
 
@@ -44,30 +61,33 @@ create table if not exists Fixture (
 TeamName = """
 create table if not exists TeamName (
 
+ seriesId integer,
  teamId integer,
  longname text,
  shortname text
 
 );"""
 
-# Game
+# AFLGame
 
-Game = """
-create table if not exists Game (
+AFLGame = """
+create table if not exists AFLGame (
 
- league text,
- year integer,
- round integer,
+ seriesId integer,
+ roundId integer,
+ matchId integer,
+ venueId integer,
 
  home_name text,
  home_score integer,
- home_logo_url text,
 
  away_name text,
  away_score integer,
- away_logo_url text,
 
- primary key (league, year, round, home_name, away_name)
+ startdatetime timestamp,
+ currentstatus text,
+
+ primary key (seriesId, roundId, matchId)
 
 );"""
 
@@ -80,10 +100,13 @@ create table if not exists FetchTime (
 
 def create_db_and_get_connection(db_name):
     # connect and re-create tables if they don't exist.
-    conn = sqlite3.connect(db_name, check_same_thread=False)
+    conn = sqlite3.connect(db_name,
+                            check_same_thread=False,
+                            detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES
+                            )
     conn.executescript(Fixture)
     conn.executescript(TeamName)
-    conn.executescript(Game)
+    conn.executescript(AFLGame)
     conn.executescript(FetchTime)
     conn.commit()
     conn.row_factory = generic_namedtuple_factory
@@ -95,31 +118,129 @@ def update_fixture(conn, records):
                             (seriesId,
                                 roundId,
                                 matchId,
+                                venueId,
                                 team_one,
                                 team_two,
-                                startdatetimeUTC)
+                                startdatetime)
                             values
                                 (:seriesId,
                                     :roundId,
                                     :matchId,
+                                    :venueId,
                                     :team_one,
                                     :team_two,
-                                    startdatetimeUTC)"""
+                                    :startdatetime)"""
     records_as_dicts = [record._asdict() for record in records]
     conn.execute(del_fixture)
     conn.executemany(insert_fixture_games, records_as_dicts)
     conn.commit()
 
+def compute_round(start_end_round_dates, today):
+    # only one round to return
+    if len(start_end_round_dates) == 1:
+        return start_end_round_dates[0]
+
+    # If the current time is within the start of the first day a game is played
+    # and midnight of the night the last game of the round is played, return
+    # that round.
+    for round in start_end_round_dates:
+        if (today < round.end.date() + ONE_DAY) and (today > round.start.date()):
+            return round
+
+    # If we are not in a round, return the last played round or the next round
+    # if we are within 1 day of the first game day (eg. Thursday for a Friday
+    # night match)
+    for round in start_end_round_dates:
+        if (round.start.date() - ONE_DAY) <= today:
+            this_round = round
+
+    return this_round
+
 def get_round(conn):
-    earliest_dates = """select seriesId, roundId, min(startdatetimeUTC)
+    start_end_round_dates = """select seriesId,
+                                roundId,
+                                min(startdatetime) as "start [timestamp]",
+                                max(startdatetime) as "end [timestamp]"
                         from Fixture
-                        group by seriesId, roundId"""
+                        group by seriesId, roundId
+                        order by seriesId, roundId"""
+    start_end_round_dates = conn.execute(start_end_round_dates).fetchall()
 
-    latest_dates = """select seriesId, roundId, max(startdatetimeUTC)
+    today = datetime.date.today()
+
+    return compute_round(start_end_round_dates, today)
+
+def refresh_AFLGame_table_round(conn, seriesId, roundId):
+    param_dict = {'seriesId': seriesId, 'roundId': roundId}
+    conn.execute("""delete from AFLGame
+                    where seriesId != :seriesId
+                        and roundId != :roundId""", param_dict)
+
+    insert_or_ignore_new_games = """insert or ignore into AFLGame
+                    (seriesId,
+                        roundId,
+                        matchId,
+                        venueId,
+                        home_name,
+                        home_score,
+                        away_name,
+                        away_score,
+                        startdatetime,
+                        currentstatus
+                        )
+                    select seriesId,
+                            roundId,
+                            matchId,
+                            venueId,
+                            team_one,
+                            null,
+                            team_two,
+                            null,
+                            StartDateTime,
+                            ''
                         from Fixture
-                        group by seriesId, roundId"""
+                        where seriesId=:seriesId
+                            and roundId=:roundId"""
 
-    
+    conn.execute(insert_or_ignore_new_games, param_dict)
+
+    conn.commit()
+
+def update_AFLGame(conn, aflgame):
+    conn.execute("""update AFLGame
+                    set home_name=:home_name,
+                        home_score=:home_score,
+                        away_name=:away_name,
+                        away_score=:away_score,
+                        currentstatus=:currentstatus
+                    where seriesId=:seriesId
+                        and roundId=:roundId
+                        and matchId=:matchId""",
+                        aflgame._asdict())
+    conn.commit()
+
+
+def get_active_games(conn):
+    result = conn.execute("""select seriesId,
+                                    roundId,
+                                    matchId,
+                                    home_name,
+                                    away_name,
+                                    venueId
+                            from AFLGame
+                            where startdatetime < datetime('now', 'localtime')
+                                and currentstatus != 'FT'
+                                """)
+
+    return result.fetchall()
+
+def get_current_round_scores(conn):
+    results = conn.execute("""select home_name,
+                                        home_score,
+                                        away_name,
+                                        away_score
+                                from AFLGame""")
+    return results.fetchall()
 
 def update_teamnamemap(conn, records):
     if not records:
@@ -127,20 +248,23 @@ def update_teamnamemap(conn, records):
 
     del_names = "delete from TeamName"
     insert_team_names = """insert into TeamName
-                            (teamId,
+                            (seriesId,
+                                teamId,
                                 shortname,
                                 longname)
                             values
-                                (:teamId,
+                                (:seriesId,
+                                :teamId,
                                 :shortname,
                                 :longname)"""
 
     records_as_dicts = [record._asdict() for record in records]
-    conn.execute(del_fixture)
-    conn.executemany(insert_fixture_games, records_as_dicts)
+    conn.execute(del_names)
+    conn.executemany(insert_team_names, records_as_dicts)
     conn.commit()
 
-def get_teamid(conn, teamname):
+def get_teamid(conn, seriesId, teamname):
     result = conn.execute("""select teamId from TeamName
-                    where longname=?""", teamname)
-    return result.fetchone().teamId
+                    where seriesId=?
+                    and longname=?""", (seriesId, teamname))
+    return result.fetchone()
